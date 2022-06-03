a77c4ef2515932b1c533a10f77ce63909b112f2f
rattesaponto1 <rattesa.ponto@work.briagro.co.id>
2022-06-03 09:48:25 +0700

    test

---
+++ Object Repository/Page_DEV Agro Banking System/a_Logout.rs
@@ -0,0 +1,139 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Logout</name>
+   <tag></tag>
+   <elementGuidId>cd8b9d38-01d4-4a4e-b6c1-7e6c7b8620b6</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[@onclick=&quot;window.top.location = 'Logout.aspx';&quot;]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>a[title=&quot;Keluar dari Aplikasi&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>b2f668ba-49f3-4d86-b72b-76b109316b71</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>onclick</name>
+      <type>Main</type>
+      <value>window.top.location = 'Logout.aspx';</value>
+      <webElementGuid>973810e8-5350-4f09-97f0-82c3859e5b94</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>title</name>
+      <type>Main</type>
+      <value>Keluar dari Aplikasi</value>
+      <webElementGuid>9c3be15e-0612-41b3-9d2b-e4a76d0fbe25</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Logout</value>
+      <webElementGuid>8f1675a1-3db0-4233-849a-0a7b24457341</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;menu1&quot;)/li[4]/a[1]</value>
+      <webElementGuid>50392c3b-0d06-4ee1-ad26-2f1e9af64fbf</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>ref_element</name>
+      <type>Main</type>
+      <value>Object Repository/Page_DEV Agro Banking System/iframe_ampltpampgtBrowser anda tidak menduk_ca06c4</value>
+      <webElementGuid>61ded196-c15d-4e22-a51f-cfb1e1e98f92</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//a[@onclick=&quot;window.top.location = 'Logout.aspx';&quot;]</value>
+      <webElementGuid>616618e8-0e0c-47bd-90c7-7d658a62bed0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//ul[@id='menu1']/li[4]/a</value>
+      <webElementGuid>fbab6c77-204b-4fe9-be25-da708ae58cd9</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>//a[contains(text(),'Logout')]</value>
+      <webElementGuid>553a371d-2db4-4d5e-bb5a-aa998a0f9155</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Migration'])[1]/following::a[1]</value>
+      <webElementGuid>b5fcfc24-3db3-45a3-9c14-4b863ddfdd89</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='General'])[1]/following::a[2]</value>
+      <webElementGuid>619ef194-ed8b-455c-ad25-0b2fe70a11dd</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)=concat('id(', '&quot;', 'menu1', '&quot;', ')/li[4]/a[1]')])[1]/preceding::a[1]</value>
+      <webElementGuid>9b18417e-56e9-4922-b66d-7a155eb654e7</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Logout']/parent::*</value>
+      <webElementGuid>e8d5bbf0-686f-41c9-8ac6-d66cce685744</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//li[4]/a</value>
+      <webElementGuid>3977e968-fe2b-49d1-b0e9-a1d5085d4034</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@title = 'Keluar dari Aplikasi' and (text() = 'Logout' or . = 'Logout')]</value>
+      <webElementGuid>5e254873-5c4c-4b4d-a41f-3a707595cd74</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
